4e746ac5c57075336c5eb2925109c9ff7bb08ef3
Bruno Pavao <brunopavao1612@gmail.com>
2025-11-27 15:03:23 -0400

    Clean up comments in login script
    Removed comments for session storage and redirection.

--- script.js
+++ script.js
@@ -8,10 +8,8 @@ function verificarLogin() {
     const senhaCorreta = "1234";
 
     if (usuario === usuarioCorreto && senha === senhaCorreta) {
-        // Armazena o login na sessão
         sessionStorage.setItem('usuarioLogado', usuario);
         
-        // Redireciona para a página inicial
         window.location.href = "PaginaInicial/index.html";
     } else {
         mensagem.textContent = "Usuário ou senha incorretos!";
